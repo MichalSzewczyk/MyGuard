056649a607c10bdd3524bbc427a4a4a514638e04
mszewczyk <szewczykm94@gmail.com>
2017-05-25 20:52:38 +0200

    .

--- src/main/java/com/guard/myguard/DangerLevel.java
+++ src/main/java/com/guard/myguard/DangerLevel.java
@@ -1,5 +1,9 @@
 package com.guard.myguard;
 
 public enum DangerLevel {
-    CHILL, GOOD, MEDIUM, HEIGHT, EXTREME, INCREDIBLE, HELL
+    CHILL, GOOD, MEDIUM, HEIGHT, EXTREME, INCREDIBLE, HELL;
+
+    public static DangerLevel getEnumByIntValue(int value){
+        return DangerLevel.values()[value];
+    }
 }
---
+++ src/main/java/com/guard/myguard/IntToEnumMapping.java
@@ -0,0 +1,8 @@
+package com.guard.myguard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntToEnumMapping {
+    public static List<DangerLevel> mapping = new ArrayList<>();
+}
