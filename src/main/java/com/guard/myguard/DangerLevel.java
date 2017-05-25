91b84467db7563e3ed7442ff5c147fc9b13ba2cc
mszewczyk <szewczykm94@gmail.com>
2017-05-25 19:57:03 +0200

    .

---
+++ src/main/java/com/guard/myguard/DangerLevel.java
@@ -0,0 +1,5 @@
+package com.guard.myguard;
+
+public enum DangerLevel {
+    CHILL, GOOD, MEDIUM, HEIGHT, EXTREME, INCREDIBLE, HELL
+}
--- src/test/java/com/guard/myguard/ExampleUnitTest.java
+++ src/test/java/com/guard/myguard/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.guard.myguard;
 
+import com.guard.myguard.model.Crime;
+import com.guard.myguard.services.interfaces.JsonRestService;
+import com.guard.myguard.services.interfaces.RestService;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +18,25 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private RestService<Crime[]> crimeRestService;
+    private static final String URL = "https://data.police.uk/api/crimes-at-location?date=2012-02&lat=52.629729&lng=-1.131592";
+    private static final String CRIMES_AT_LOCATION_REQUEST = "crimes-at-location?date=2012-02&lat=52.629729&lng=-1.131592";
+
+    @Before
+    public void prepareForTests() {
+        this.crimeRestService = new JsonRestService<>(URL, Crime[].class);
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testIfRestServiceWorksProperly() throws Exception {
+        String jsonData = crimeRestService.getJsonForRequest(CRIMES_AT_LOCATION_REQUEST);
+        System.out.println(jsonData);
+    }
+
+    @Test
+    public void testIfRestServiceParsesRequestedDataProperly() throws Exception {
+        String jsonData = crimeRestService.getJsonForRequest(CRIMES_AT_LOCATION_REQUEST);
+        Crime[] crimes = crimeRestService.parseJsonData(jsonData);
+        System.out.println(Arrays.toString(crimes));
     }
 }
