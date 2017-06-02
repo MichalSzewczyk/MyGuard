377cc98b0c92b90418a9ae087d153939af5de769
mszewczyk <szewczykm94@gmail.com>
2017-06-02 19:02:26 +0200

    .

---
+++ src/main/java/com/guard/myguard/utils/Tuple.java
@@ -0,0 +1,23 @@
+package com.guard.myguard.utils;
+
+/**
+ * Created by DELL on 2017-06-02.
+ */
+
+public final class Tuple<K, V> {
+    private final K key;
+    private final V value;
+
+    public Tuple(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
