ffa8a54180708c392e0d8476b9e7599c47025beb
mszewczyk <szewczykm94@gmail.com>
2017-06-02 20:39:08 +0200

    .

---
+++ src/main/java/com/guard/myguard/services/interfaces/ParsingService.java
@@ -0,0 +1,24 @@
+package com.guard.myguard.services.interfaces;
+import java.io.IOException;
+import java.io.Serializable;
+
+
+
+public interface ParsingService {
+    /**
+     * Method is responsible for serializing object to json
+     *
+     * @param object is object to serialize
+     * @return json representation of serialized object
+     */
+    String serialize(Serializable object);
+
+    /**
+     * Method is responsible for deserialization object from json
+     *
+     * @param json is string representation of serialized object
+     * @param type is class to which we want to deserialize json
+     * @return deserialized object
+     */
+    <T> T deserialize(String json, Class<T> type);
+}
