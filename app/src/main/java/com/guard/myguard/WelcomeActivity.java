625762c4deba09e9940abedec1e36db637b7ac60
mszewczyk <szewczykm94@gmail.com>
2017-06-09 10:51:04 +0200

    .

--- app/src/main/java/com/guard/myguard/WelcomeActivity.java
+++ app/src/main/java/com/guard/myguard/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.guard.myguard;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,13 @@ import com.guard.myguard.database.DBOpenHelper;
 import com.guard.myguard.services.impl.StoredLoginHandler;
 import com.guard.myguard.services.interfaces.LoginHandler;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.SEND_SMS;
+import static android.Manifest.permission.USE_FINGERPRINT;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission_group.STORAGE;
 import static com.guard.myguard.utils.Utils.invokeStartActivity;
 
 public class WelcomeActivity extends Activity {
@@ -52,6 +60,28 @@ public class WelcomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.openHelper = new DBOpenHelper(this);
         setContentView(R.layout.activity_welcome);
+
+        if (checkSelfPermission(CAMERA)
+                == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(STORAGE)
+                        == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(SEND_SMS)
+                        == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(USE_FINGERPRINT)
+                        == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_DENIED ||
+                checkSelfPermission(WRITE_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_DENIED) {
+
+            Log.d("permission", "permission denied to CAMERA - requesting it");
+            String[] permissions = {CAMERA, STORAGE, SEND_SMS, READ_EXTERNAL_STORAGE,
+                    USE_FINGERPRINT, ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE};
+
+            requestPermissions(permissions, 1);
+        }
 
         initComponents();
         setListeners();
